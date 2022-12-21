e4cc66debe64e8860d6d14b7164e3c0911439ecb
draurkh <talha760seckin@gmail.com>
2022-12-21 19:44:19 +0300

    django secret key is hidden

--- Web/climbeeweb/climbeeweb/asgi.py
+++ Web/climbeeweb/climbeeweb/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for climbee project.
+ASGI config for climbeeweb project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'climbee.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'climbeeweb.settings')
 
 application = get_asgi_application()
--- Web/climbeeweb/climbeeweb/wsgi.py
+++ Web/climbeeweb/climbeeweb/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for climbee project.
+WSGI config for climbeeweb project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'climbee.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'climbeeweb.settings')
 
 application = get_wsgi_application()
